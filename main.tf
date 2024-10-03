09b43d2d7086dc37180b877ae5256b18d230a1ac
ohidalgo <101749097+rduserjob@users.noreply.github.com>
2024-10-03 11:01:41 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,62 +1,49 @@
 # main.tf
 
-# Configurar el proveedor de GCP
+# Configuring GCP Provider
 provider "google" {
-  project = "my-pagerduty-demo-project-v01"  # Reemplaza con el ID de tu proyecto
-  region  = "us-central1"     # Reemplaza con la región deseada
-  zone    = "us-central1-a"   # Reemplaza con la zona deseada
+  project = "my-pagerduty-demo-project-v01"  
+  region  = "us-central1"    
+  zone    = "us-central1-a"   #
 }
 
-# Crear una red de VPC
-resource "terraform_google_compute_network" "vpc_network" {
-  name = "terraform-vpc"
-}
-
-# Crear una subred
-resource "terraform_google_compute_subnetwork" "subnetwork" {
-  name          = "mi-subred"
-  region        = "us-central1"    # Reemplaza con la región deseada
-  network       = terraform_google_compute_network.vpc_network.id
-  ip_cidr_range = "10.0.0.0/24"
-}
-
-# Crear una instancia de VM
-resource "terraform_google_compute_instance" "Terraform_vm" {
-  name         = "Terraform-vm"
-  machine_type = "e2-micro"  # Puedes elegir el tipo de máquina que prefieras
+# Creating vm
+resource "google_compute_instance" "mi_vm" {
+  name         = "Terraform-Vm"
+  machine_type = "e2-micro"  
   zone         = "us-central1-a"
 
-  # Conectar la instancia a la red
+  # network and sub-network
   network_interface {
-    network    = terraform_google_compute_network.vpc_network.name
-    subnetwork = terraform_google_compute_subnetwork.subnetwork.name
+    network    = "default"
+    subnetwork = "default"
 
     access_config {
-      # Esto asigna una IP pública
+      # Public IP
     }
   }
 
-  # Especificar la imagen del sistema operativo
+  # Disk and SO
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"  # Puedes cambiar la imagen por la que prefieras
+      image = "debian-cloud/debian-11"  
     }
   }
 
-  # Metadata opcional, como startup scripts
+  # Metadata startup scripts
   metadata_startup_script = <<-EOT
     #!/bin/bash
-    echo "Hola, mundo desde mi VM!" > /var/tmp/hola_mundo.txt
+    echo "Hello,world since my VM!" > /var/tmp/hello_world.txt
   EOT
 
-  # Etiquetas de red para el firewall
-  tags = ["http-server"]
+  # tags
+  tags = ["terraform-http-server"]
 }
 
-# Crear reglas de firewall para permitir el tráfico HTTP y SSH
-resource "google_compute_firewall" "default" {
+# firewall tools
+resource "google_compute_firewall" "allow-http-ssh" {
   name    = "allow-http-ssh"
-  network = google_compute_network.vpc_network.name
+  network = "default"
 
   allow {
     protocol = "tcp"
